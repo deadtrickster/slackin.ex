a633e0ab1c378234ad551a938ca19026c8f8606c
Ilya Khaprov <dead.trickster@gmail.com>
2017-04-07 00:34:41 +0300

    or not?

--- config/prod.exs
+++ config/prod.exs
@@ -17,7 +17,7 @@ config :slackin_ex, SlackinEx.Web.Endpoint,
   on_init: {SlackinEx.Web.Endpoint, :load_from_system_env, []},
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
