dedb7faa9ae8d8a0153905f6be4a39bae0d6413f
Ilya Khaprov <dead.trickster@gmail.com>
2017-04-09 17:22:37 +0300

    ignore error if prod.secret.exs doesn't exist

--- config/prod.release.exs
+++ config/prod.release.exs
@@ -3,6 +3,17 @@ use Mix.Config
 config :slackin_ex, SlackinEx.Web.Endpoint,
   server: true
 
-if File.exists?("prod.secret.exs") do
+IO.puts(System.cwd())
+
+try do
   import_config "prod.secret.exs"
+rescue
+  e in Mix.Config.LoadError ->
+    case e do
+      %Mix.Config.LoadError{error: %File.Error{}} ->
+        :ok
+      _ ->
+        reraise(e, System.stacktrace)
+    end
 end
+
