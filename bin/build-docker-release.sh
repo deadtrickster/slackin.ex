8c267356ec2fe37a4327edcfa0f10e4aaa374d95
Ilya Khaprov <dead.trickster@gmail.com>
2017-05-05 12:27:17 +0300

    default docker tag is deadtrickster/slackin_ex

--- bin/build-docker-release.sh
+++ bin/build-docker-release.sh
@@ -9,4 +9,4 @@ docker cp $CID:/tmp/slackin_ex/_build/prod/rel/slackin_ex $TEMP
 docker stop $CID
 docker rm $CID
 cp Dockerfile $TEMP
-docker build $TEMP -t $1 -f $TEMP/Dockerfile
+docker build $TEMP -t ${1:-deadtrickster/slackin_ex} -f $TEMP/Dockerfile
