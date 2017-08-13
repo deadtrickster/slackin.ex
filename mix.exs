94ffbcaa2aa8afe62b798ca7733d6c56774b0055
Ilya Khaprov <dead.trickster@gmail.com>
2017-08-13 23:39:42 +0300

    Bump to v0.1.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule SlackinEx.Mixfile do
 
   def project() do
     [app: :slackin_ex,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.3",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
